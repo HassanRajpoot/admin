3477ab34b6800cd0cd0b5af0a2528c41150764b6
HassanRajpoot <xhah450@gmail.com>
2024-02-05 23:22:20 +0500

    Admin panel for ecommerce site

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+const Customers = lazy(() => import("./pages/Customers"));
+const NewProduct = lazy(() => import("./pages/management/NewProduct"));
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} /> 
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/product/new" element={<NewProduct/>}/>
+          <Route path="/admin/product/:id" element={<NewProduct/>}/>
+          <Route path="/admin/transaction/:id" element={<NewProduct/>}/>
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
